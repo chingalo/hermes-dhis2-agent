ef2eccdbf841cc8870259bbf49280da2c081c47e
Joseph Chingalo <profschingalo@gmail.com>
2026-05-07 17:22:31 +0300

    feat: add initial implementation of starApp function for application startup and logging

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { LogsUtil } from './utils';
+import { AppProcess } from './app';
+
+starApp();
+
+async function starApp() {
+  const logsUtil = new LogsUtil();
+  await logsUtil.clearLogs();
+  await logsUtil.addLogs('info', 'start an app', 'app');
+  try {
+    const appProcess = new AppProcess();
+    await appProcess.startAppProcess();
+  } catch (error: any) {
+    error = error.message || error;
+    console.log({ error });
+  }
+  await logsUtil.addLogs('info', 'End of script', 'app');
+}
